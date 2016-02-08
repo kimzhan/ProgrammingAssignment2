abc44165881e9bdceef1696184517d8709b63f2f
XinDong Li <xindongli@XinDongdeMacBook-Pro.local>
2016-02-07 19:15:56 -0700

    2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,96 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+R version 3.2.3 (2015-12-10) -- "Wooden Christmas-Tree"
+Copyright (C) 2015 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin13.4.0 (64-bit)
 
-makeCacheMatrix <- function(x = matrix()) {
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
 
-}
+  Natural language support but running in an English locale
 
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
 
-## Write a short comment describing this function
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+[R.app GUI 1.66 (7060) x86_64-apple-darwin13.4.0]
+
+[History restored from /Users/xindongli/.Rapp.history]
+
+> makeCacheMatrix <- function(x = matrix()) {
++         inv <- NULL
++         set <- function(y) {
++                 x <<- y
++                 inv <<- NULL
++         }
++         get <- function() x
++         setInverse <- function(inverse) inv <<- inverse
++         getInverse <- function() inv
++         list(set = set,
++              get = get,
++              setInverse = setInverse,
++              getInverse = getInverse)
++ }
+> 
+> cacheSolve <- function(x, ...) {
++         ## Return a matrix that is the inverse of 'x'
++         inv <- x$getInverse()
++         if (!is.null(inv)) {
++                 message("getting cached data")
++                 return(inv)
++         }
++         mat <- x$get()
++         inv <- solve(mat, ...)
++         x$setInverse(inv)
++         inv
++ }
+> 
+>  my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+>  my_matrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+>  my_matrix$getInverse()
+NULL
+>  cacheSolve(my_matrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+>  cacheSolve(my_matrix)
+getting cached data
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> getting cached data
+Error: unexpected symbol in "getting cached"
+> my_matrix$getInverse()
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+>  my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+> my_matrix$get()
+     [,1] [,2]
+[1,]    2    1
+[2,]    2    4
+> my_matrix$getInverse()
+NULL
+> cacheSolve(my_matrix)
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> cacheSolve(my_matrix)
+getting cached data
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> getting cached data
+Error: unexpected symbol in "getting cached"
+> my_matrix$getInverse()
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> 
